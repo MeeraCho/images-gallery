e2b71476a7e952778ae5162cc062a701a75db9cf
MiraCho <miracho119@gmail.com>
2023-08-24 19:40:21 -0600

    controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+};
+
 const Header = ({ title }) => {
     return (
-        <Navbar bg="light" data-bs-theme="light" variant="light">
+        <Navbar style={navbarStyle} variant="light">
             <Container>
                 <Navbar.Brand href="/">{title}</Navbar.Brand>
             </Container>
