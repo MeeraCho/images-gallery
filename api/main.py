2d8460936ee33e0588192f72ad99ed3e7055526b
MiraCho <miracho119@gmail.com>
2023-09-03 23:09:21 -0600

    removed other_module.py

--- api/main.py
+++ api/main.py
@@ -1,12 +1,10 @@
 from flask import Flask 
 from other_module import fn_from_other_module
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-fn_from_other_module()
-
-# @app.route("/")
-# def hello():
-#     return "Hello, World!"
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
 app.run(port=5000, debug=True)
